b5f3d0c22056e1c6dfa9172e9ae9bbe166acbfa4
Ravish Kumar <ravish99055@gmail.com>
2025-05-05 15:17:55 +0530

    Update bookController.js

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,99 +1,102 @@
-const product = require('../db/Models/bookModels');
-const productModel = require('../db/Models/bookModels')
 
+const productModel = require('../db/Models/bookModels');
 
-const createBooks = async(req, res) => {
-    const {name,author, price, pages, genre, year} =  req.body;
+const createBooks = async (req, res) => {
+    try {
+        const { name, author, price, pages, genre, year } = req.body;
 
-    const product = new productModel({name, author, price, pages,genre, year});
+        const newBook = new productModel({ name, author, price, pages, genre, year });
+        await newBook.save();
 
-    
-
-    await product.save();
-
-      res.status(200).json({success: true, message: "book is saved", data : product})
-
-
-}
-
-const getAllBooks = async(req,res) => {
-    const products = await productModel.find({});
-    if (!products) {
-        res.status(400).json({success: false, message: "book not found"})
+        return res.status(201).json({ success: true, message: "Book saved successfully", data: newBook });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
+};
 
-    res.status(200).json({success: true, message: "books fetched", data : products})
-
-
-}
-
-const updateBook = async(req,res) => {
-    const productId = req.params.id;
-
-    const updatedProduct = await productModel.findByIdAndUpdate(productId, req.body);
-
-
-    if (!updatedProduct) {
-        res.status(400).json({success: false, message: "book not found"})
+const getAllBooks = async (req, res) => {
+    try {
+        const books = await productModel.find({});
+        return res.status(200).json({ success: true, message: "Books fetched", data: books });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
+};
 
-    res.status(200).json({success: true, message: "book is updated", data : updatedProduct})
+const updateBook = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const updatedBook = await productModel.findByIdAndUpdate(productId, req.body, { new: true });
 
-}
+        if (!updatedBook) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
 
-const deleteBook = async(req,res) => {
-    const productId = req.params.id;
-    const deletedProduct = productModel.findByIdAndDelete(productId);
-
-    if (!deletedProduct) {
-        res.status(400).json({success: false, message: "book not found"})
+        return res.status(200).json({ success: true, message: "Book updated", data: updatedBook });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
+};
 
-    res.status(200).json({success: true, message: "book is deleted", data : deleteProduct})
-}
+const deleteBook = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const deletedBook = await productModel.findByIdAndDelete(productId);
 
-const getByAuthor = async(req,res) => {
-      const authorId = req.params.id;
-      const book = await productModel.findOne({author : req.body.authorId})
-      if (!authorId) {
-        res.status(404).json({success: false, message : "author not found"})
-      }
-     
-      if(!book) {
-        res.status(400).json({success: false, message: "Book is not present"})
+        if (!deletedBook) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
+
+        return res.status(200).json({ success: true, message: "Book deleted", data: deletedBook });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
-      res.status(200).json({success: true, message : "book fetched", data: book})
-}
-const getByTitle = async(req,res) => {
-      const name = req.params.id;
-      const book = await productModel.findOne({name : req.body.name})
-      if (!name) {
-        res.status(404).json({success: false, message : "book not found"})
-      }
+};
 
-      
-    if(!book) {
-        res.status(400).json({success: false, message: "Book is not present"})
+const getByAuthor = async (req, res) => {
+    try {
+        const author = req.params.author;
+        const books = await productModel.find({ author });
+
+        if (!books || books.length === 0) {
+            return res.status(404).json({ success: false, message: "No books found for this author" });
+        }
+
+        return res.status(200).json({ success: true, message: "Books fetched", data: books });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
-      res.status(200).json({success: true, message : "book fetched", data: book})
-}
+};
 
-getBookById = async(req,res) => {
-    const id = req.params.id;
-    const book = await productModel.findOne({name : req.body._id})
-    if (!id) {
-      res.status(404).json({success: false, message : "book not found"})
+const getByTitle = async (req, res) => {
+    try {
+        const title = req.params.name;
+        const book = await productModel.findOne({ name: title });
+
+        if (!book) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
+
+        return res.status(200).json({ success: true, message: "Book fetched", data: book });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
     }
+};
 
-    
-  if(!book) {
-      res.status(400).json({success: false, message: "Book is not present"})
-  }
-    res.status(200).json({success: true, message : "book fetched", data: book})
-    
-}
+const getBookById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const book = await productModel.findById(id);
 
+        if (!book) {
+            return res.status(404).json({ success: false, message: "Book not found" });
+        }
 
+        return res.status(200).json({ success: true, message: "Book fetched", data: book });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Server error", error: err.message });
+    }
+};
 
 module.exports = {
     createBooks,
@@ -103,5 +106,4 @@ module.exports = {
     getByAuthor,
     getByTitle,
     getBookById
-}
-
+};
